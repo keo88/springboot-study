dd28aa8de759f64d897f3cc67be96a61c12f809e
keo-kim <keo.kim@navercorp.com>
2024-11-12 13:32:10 +0900

    feat: add index controller.

---
+++ src/main/java/com/keokim/springboot/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.keokim.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class IndexController {
+
+	@GetMapping("/")
+	public String getIndex() {
+		return "Hello World";
+	}
+}
