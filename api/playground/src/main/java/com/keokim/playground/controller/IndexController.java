8083fb8027a8d71896043c033512e532718a6486
keo-kim <keo.kim@navercorp.com>
2024-11-19 18:32:14 +0900

    feat: add view template engine thymeleaf and add index.html.

--- api/playground/src/main/java/com/keokim/playground/controller/IndexController.java
+++ api/playground/src/main/java/com/keokim/playground/controller/IndexController.java
@@ -1,19 +1,24 @@
 package com.keokim.playground.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RestController
+@Controller
 public class IndexController {
 
 	@GetMapping("/")
-	public String getIndex() {
-		return "Hello World";
+	public String getIndex(Model model) {
+		model.addAttribute("username", "손님");
+
+		return "index";
 	}
 
+	@ResponseBody
 	@GetMapping("/health-check")
 	public String healthCheck() {
 		return "OK";
