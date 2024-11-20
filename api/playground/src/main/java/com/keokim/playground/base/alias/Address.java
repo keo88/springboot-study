a63d9db146b56857e1eba1e9d9f34d6a61fc85c8
keo-kim <keo.kim@navercorp.com>
2024-11-20 18:12:59 +0900

    fix: make address class immutable.

--- api/playground/src/main/java/com/keokim/playground/base/alias/Address.java
+++ api/playground/src/main/java/com/keokim/playground/base/alias/Address.java
@@ -1,14 +1,18 @@
 package com.keokim.playground.base.alias;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Embeddable
 @Getter
-@Setter
+@AllArgsConstructor
 public class Address {
 	private String city;
 	private String street;
 	private String zipcode;
+
+	protected Address() {
+
+	}
 }
