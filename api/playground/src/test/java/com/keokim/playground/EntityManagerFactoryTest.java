a30047d000fbe5a6f5d14d013dd961ff6af67095
keo-kim <keo.kim@navercorp.com>
2024-11-20 17:53:22 +0900

    feat: add test for jpa entity.

--- api/playground/src/test/java/com/keokim/playground/EntityManagerFactoryTest.java
+++ api/playground/src/test/java/com/keokim/playground/EntityManagerFactoryTest.java
@@ -1,11 +1,14 @@
 package com.keokim.playground;
 
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.keokim.playground.base.alias.Member;
+import com.keokim.playground.base.alias.PurchaseOrder;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -23,14 +26,12 @@ public class EntityManagerFactoryTest {
 		final EntityManager entityManager = entityManagerFactory.createEntityManager();
 		final EntityTransaction transaction = entityManager.getTransaction();
 
-		try {
+		try (entityManager) {
 			transaction.begin();
 			executeTestBusinessLogic(entityManager);
 			transaction.commit();
 		} catch (Exception e) {
 			transaction.rollback();
-		} finally {
-			entityManager.close();
 		}
 	}
 
@@ -38,6 +39,7 @@ public class EntityManagerFactoryTest {
 		final Member newMember = new Member();
 		newMember.setName("Keo Kim");
 		newMember.setPassword("password");
+		newMember.setCreatedAt(LocalDateTime.now());
 
 		entityManager.persist(newMember);
 		final Member foundMember = entityManager.find(Member.class, 1L);
@@ -46,5 +48,30 @@ public class EntityManagerFactoryTest {
 
 		Assertions.assertEquals(newMember, foundMember);
 		Assertions.assertEquals(newMember, query.getSingleResult());
+	}
+
+	@Test
+	public void testEntityManagerFactory2() {
+		final EntityManager entityManager = entityManagerFactory.createEntityManager();
+		final EntityTransaction transaction = entityManager.getTransaction();
+
+		Member member = new Member();
+		member.setName("Keo Kim");
+		member.setPassword("password");
+		member.setCreatedAt(LocalDateTime.now());
+		PurchaseOrder order = new PurchaseOrder();
+		order.setMember(member);
+
+		try {
+			transaction.begin();
+			entityManager.persist(member);
+			entityManager.persist(order);
+			final TypedQuery<Member> query = entityManager.createQuery("select m from Member m", Member.class);
+			Assertions.assertEquals(member, query.getSingleResult());
+			transaction.commit();
+		} finally {
+			entityManager.close();
+		}
+
 	}
 }
