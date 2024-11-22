2881fe50124bb3e301b997cf9a5dc713976e95f4
keo-kim <keo.kim@navercorp.com>
2024-11-22 16:56:07 +0900

    feat: add MemberRepository.

---
+++ api/playground/src/main/java/com/keokim/playground/repository/MemberRepository.java
@@ -0,0 +1,37 @@
+package com.keokim.playground.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.keokim.playground.base.alias.Member;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	public void save(Member member) {
+		em.persist(member);
+	}
+
+	public Optional<Member> findById(long id) {
+		return Optional.ofNullable(em.find(Member.class, id));
+	}
+
+	public List<Member> findAll() {
+		return em.createQuery("select m from Member m", Member.class).getResultList();
+	}
+
+	public List<Member> findByName(String name) {
+		return em.createQuery("select m from Member m where m.name = :name", Member.class)
+			.setParameter("name", name)
+			.getResultList();
+	}
+
+}
