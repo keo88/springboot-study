758b94e8c116bc14d94b3f61b36910df335259f9
keo-kim <keo.kim@navercorp.com>
2024-11-22 10:42:35 +0900

    test: add test for cascade.

--- api/playground/src/test/java/com/keokim/playground/EntityManagerFactoryTest.java
+++ api/playground/src/test/java/com/keokim/playground/EntityManagerFactoryTest.java
@@ -1,12 +1,13 @@
 package com.keokim.playground;
 
-import java.time.LocalDateTime;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 
+import com.keokim.playground.base.alias.Delivery;
+import com.keokim.playground.base.alias.DeliveryStatus;
 import com.keokim.playground.base.alias.Member;
 import com.keokim.playground.base.alias.PurchaseOrder;
 
@@ -14,12 +15,16 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
 
 @SpringBootTest
 public class EntityManagerFactoryTest {
 
 	@Autowired
 	private EntityManagerFactory entityManagerFactory;
+
+	@Autowired
+	private EntityManager entityManager;
 
 	@Test
 	public void testEntityManagerFactory() {
@@ -39,7 +44,7 @@ public class EntityManagerFactoryTest {
 		final Member newMember = new Member();
 		newMember.setName("Keo Kim");
 		newMember.setPassword("password");
-		newMember.setCreatedAt(LocalDateTime.now());
+		// newMember.setCreatedAt(LocalDateTime.now());
 
 		entityManager.persist(newMember);
 		final Member foundMember = entityManager.find(Member.class, 1L);
@@ -58,7 +63,7 @@ public class EntityManagerFactoryTest {
 		Member member = new Member();
 		member.setName("Keo Kim");
 		member.setPassword("password");
-		member.setCreatedAt(LocalDateTime.now());
+		// member.setCreatedAt(LocalDateTime.now());
 		PurchaseOrder order = new PurchaseOrder();
 		order.setMember(member);
 
@@ -74,4 +79,61 @@ public class EntityManagerFactoryTest {
 		}
 
 	}
+
+	@Transactional
+	@Commit
+	@Test
+	public void testBiDirectionalEntityMapping() {
+		Member member = new Member();
+		member.setName("Keo Kim");
+		member.setPassword("password");
+
+		PurchaseOrder purchaseOrder = new PurchaseOrder();
+		purchaseOrder.setMember(member);
+
+		Delivery delivery = new Delivery();
+		delivery.setStatus(DeliveryStatus.READY);
+		// delivery.setPurchaseOrder(purchaseOrder);
+
+		purchaseOrder.setDelivery(delivery);
+
+		// entityManager.persist(purchaseOrder);
+		entityManager.persist(delivery);
+
+		Assertions.assertNotNull(delivery.getId());
+		Delivery foundDelivery = entityManager.createQuery("SELECT d FROM Delivery d", Delivery.class).getSingleResult();
+		Assertions.assertEquals(delivery, foundDelivery);
+		Assertions.assertEquals(purchaseOrder.getDelivery(), foundDelivery);
+
+		Assertions.assertNull(foundDelivery.getPurchaseOrder());
+		Assertions.assertNotNull(purchaseOrder.getDelivery());
+
+	}
+
+	@Transactional
+	@Test
+	public void testCascadingPersistence() {
+		Member member = new Member();
+		member.setName("Keo Kim");
+		member.setPassword("password");
+
+		PurchaseOrder purchaseOrder = new PurchaseOrder();
+		purchaseOrder.setMember(member);
+
+		Delivery delivery = new Delivery();
+		delivery.setStatus(DeliveryStatus.READY);
+		// delivery.setPurchaseOrder(purchaseOrder);
+
+		purchaseOrder.setDelivery(delivery);
+
+		entityManager.persist(purchaseOrder);
+		entityManager.persist(delivery);
+		// entityManager.persist(member);
+
+		// Member is initialized.
+		// Member foundMember = entityManager.createQuery("SELECT m FROM Member m WHERE m.name = 'Keo Kim'", Member.class).getSingleResult();
+		// Assertions.assertEquals(member, foundMember);
+		Assertions.assertNotNull(member.getId());
+	}
+
 }
