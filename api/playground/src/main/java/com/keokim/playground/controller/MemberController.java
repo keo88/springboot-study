de719c4d19dbb983b2c14f6df48d9b14775b9bb1
keo-kim <keo.kim@navercorp.com>
2024-12-06 17:34:39 +0900

    feat: add exception handling when form validation fails.

--- api/playground/src/main/java/com/keokim/playground/controller/MemberController.java
+++ api/playground/src/main/java/com/keokim/playground/controller/MemberController.java
@@ -1,11 +1,15 @@
 package com.keokim.playground.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.keokim.playground.base.alias.Member;
 import com.keokim.playground.base.dto.MemberForm;
 import com.keokim.playground.service.MemberService;
 
@@ -33,14 +37,23 @@ public class MemberController {
 	}
 
 	@PostMapping("/member/new")
-	public String create(@Valid MemberForm form) {
+	public String create(@Valid MemberForm form, BindingResult bindingResult) {
+
+		if (bindingResult.hasErrors()) {
+			log.info("errors={}", bindingResult);
+			return "member/createMemberForm";
+		}
+
 		memberService.join(form);
 		return "redirect:/";
 	}
 
 	@ResponseBody
-	@GetMapping("/member/list")
-	public String getList() {
+	@GetMapping("/member")
+	public String getList(Model model) {
+		List<Member> members = memberService.findMembers();
+		model.addAttribute("members", members);
+
 		return "member/memberList";
 	}
 
