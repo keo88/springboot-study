ff1e70914c7d1f513c41e63d675a4dfa0d91091b
keo-kim <keo.kim@navercorp.com>
2024-12-11 17:50:46 +0900

    feat: show alert message on failed login attempt.

--- api/playground/src/main/java/com/keokim/playground/controller/MemberController.java
+++ api/playground/src/main/java/com/keokim/playground/controller/MemberController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.keokim.playground.base.alias.Member;
@@ -25,8 +26,13 @@ public class MemberController {
 	private final MemberService memberService;
 
 	@GetMapping("/member/login")
-	public String login(Model model) {
+	public String login(Model model, @RequestParam(value = "error", required = false) String error) {
 		model.addAttribute("memberForm", new MemberForm());
+
+		if (error != null) {
+			model.addAttribute("error", error);
+		}
+
 		return "member/login";
 	}
 
