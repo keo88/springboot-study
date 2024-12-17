fb984d57cbf246d53eaedef3bce13b9980dcd4d4
keo-kim <keo.kim@navercorp.com>
2024-12-17 11:27:23 +0900

    feat: add health check.

--- api/ncphw/src/main/java/com/keokim/ncphw/controller/IndexController.java
+++ api/ncphw/src/main/java/com/keokim/ncphw/controller/IndexController.java
@@ -3,13 +3,19 @@ package com.keokim.ncphw.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String getPage()
-    {
+    public String getPage() {
         return "redirect:/server";
+    }
+
+    @GetMapping("/health-check")
+    @ResponseBody
+    public String healthCheck() {
+        return "OK";
     }
 }
